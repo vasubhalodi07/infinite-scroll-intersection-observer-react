115cc0bb02c52178175a4797e97378eb66eced0d
Vasu Bhalodi <vasubhalodi111@gmail.com>
2023-12-30 11:55:14 +0530

    solve: call api twice in inital phase in useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,17 @@ import { Provider } from "react-redux";
 import store from "./app/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
 );
+
+/*
+ Note: After removing the comment tag in React that enforces strict mode,
+       I've noticed an occurrence where the API is being called twice instead 
+       of once. This behavior was not observed when the tag was active 
+       during the development phase. Your feedback on this matter would be 
+       greatly appreciated.
+*/
